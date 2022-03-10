75f0756317965cc346e71b154a0fdb8358175daa
Anushka Kamalja <anushkakamalja@gmail.com>
2022-03-10 21:57:46 +0530

    Final commit
    Signed-off-by: Anushka Kamalja <anushkakamalja@gmail.com>

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.environ.get("SECRET_KEY")
+    # SECRET_KEY='PyTrio'
     SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
+    # SQLALCHEMY_DATABASE_URI='sqlite:///db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
+    # SQLALCHEMY_TRACK_MODIFICATIONS=False
